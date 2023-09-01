7eadabccb7469a9658b19888ffe230802d5f588b
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2023-09-01 11:18:21 +0000

    portfolio update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that.
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science Student studying at Mahakal Institute of Technology Ujjain Madhya Pradesh. I enjoy creating unique and simplistic user interfaces in creative ways and developing websites";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Graphic Design",
+  "Web Developer",
+  "JavaScript",
+  "C/C++",
+  "Front- End Developer",
 ];
 
 /**
